a6cc8ea7c2a24987ae0815439b863957cca7ffd6
Zsuzsanna Erdos <ezsana@gmail.com>
2019-04-05 07:50:49 +0200

    Exercise ready

--- data_manager.py
+++ data_manager.py
@@ -1,6 +1,6 @@
 import database_common
 
-
+'''
 @database_common.connection_handler
 def get_mentor_names_by_first_name(cursor, first_name):
     cursor.execute("""
@@ -8,5 +8,61 @@ def get_mentor_names_by_first_name(cursor, first_name):
                     WHERE first_name = %(first_name)s ORDER BY first_name;
                    """,
                    {'first_name': first_name})
+    names = cursor.fetchall()
+    return names
+'''
+@database_common.connection_handler
+def get_mentor_names(cursor):
+    cursor.execute("""
+                    SELECT first_name, last_name FROM mentors;
+                   """)
+    names = cursor.fetchall()
+    return names
+
+@database_common.connection_handler
+def mentor_nicknames(cursor):
+    cursor.execute("""
+                        SELECT nick_name FROM mentors
+                        where city = 'Miskolc';
+                       """)
+    names = cursor.fetchall()
+    return names
+
+@database_common.connection_handler
+def carol(cursor):
+    cursor.execute("""
+                        SELECT first_name, last_name, phone_number FROM applicants
+                        where first_name = 'Carol';
+                       """)
+    names = cursor.fetchall()
+    return names
+
+
+@database_common.connection_handler
+def adipiscingenimmi(cursor):
+    cursor.execute("""
+                        SELECT CONCAT(first_name,' ', last_name) as fullname, phone_number FROM applicants
+                        where email like '%@adipiscingenimmi.edu';
+                       """)
+    names = cursor.fetchall()
+    return names
+
+
+@database_common.connection_handler
+def markus(cursor):
+    cursor.execute("""
+                        SELECT * FROM applicants
+                        where application_code = 54823;
+                       """)
+    names = cursor.fetchall()
+    return names
+
+
+@database_common.connection_handler
+def jemima(cursor):
+    cursor.execute("""
+                        SELECT phone_number FROM applicants
+                        where first_name = 'Jemima' and last_name = 'Foreman';
+                       """)
     names = cursor.fetchall()
     return names
--- main.py
+++ main.py
@@ -12,9 +12,39 @@ def index():
 @app.route('/mentors-with-best-first-name')
 def mentor_names():
     # We get back dictionaries here (for details check 'database_common.py')
-    mentor_names = data_manager.get_mentor_names_by_first_name('László')
+    mentor_names = data_manager.get_mentor_names()
+    #mentor_names = data_manager.get_mentor_names_by_first_name('László')
 
     return render_template('mentor_names.html', mentor_names=mentor_names)
+
+
+@app.route('/mentors-with-nicknames')
+def mentor_nicknames():
+    mentor_nicknames = data_manager.mentor_nicknames()
+    return render_template('mentor_nicknames.html', mentor_nicknames=mentor_nicknames)
+
+
+@app.route('/carol')
+def carol():
+    girl_with_hat = data_manager.carol()
+    return render_template('carol.html', girl_with_hat=girl_with_hat)
+
+
+@app.route('/girl_with_adipiscingenimmi')
+def adipiscingenimmi():
+    girl_with_email = data_manager.adipiscingenimmi()
+    return render_template('girl_with_adipiscingenimmi.html', girl_with_email=girl_with_email)
+
+@app.route('/markus')
+def markus():
+    new_guy = data_manager.markus()
+    return render_template('markus.html', new_guy=new_guy)
+
+
+@app.route('/jemima')
+def jemima():
+    girl = data_manager.jemima()
+    return render_template('jemima.html', girl=girl)
 
 
 if __name__ == '__main__':
