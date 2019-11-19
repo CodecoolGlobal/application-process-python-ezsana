7c1ce64fd2386b850f341719e29c068ab6298ed6
Zsuzsanna Erdos <ezsana@gmail.com>
2019-11-19 17:10:25 +0100

    H1 color changed to dark red

--- data_manager.py
+++ data_manager.py
@@ -1,6 +1,6 @@
 import database_common
 
-
+'''
 @database_common.connection_handler
 def get_mentor_names_by_first_name(cursor, first_name):
     cursor.execute("""
@@ -10,3 +10,59 @@ def get_mentor_names_by_first_name(cursor, first_name):
                    {'first_name': first_name})
     names = cursor.fetchall()
     return names
+'''
+
+@database_common.connection_handler
+def get_tables(cursor, table_name):
+    cursor.execute(f"""
+                    SELECT first_name, last_name FROM {table_name}""")
+    names = cursor.fetchall()
+    return names
+
+@database_common.connection_handler
+def nicknames(cursor, location):
+    cursor.execute(f"""
+                    SELECT concat(first_name, ' ', last_name) as fullname, nick_name from mentors
+                    where city = '{location}'""")
+    names = cursor.fetchall()
+    return names
+
+@database_common.connection_handler
+def search_for_applicant(cursor, boxes, input_data):
+    infos = [data for data in boxes if boxes[data]]
+    cursor.execute(f"""
+                    SELECT concat(first_name, ' ', last_name) as fullname, phone_number from applicants
+                    where {infos[0]} like '%{input_data}%'""")
+    names = cursor.fetchall()
+    return names
+
+
+@database_common.connection_handler
+def add_new_applicant(cursor, datas):
+    new_datas = [datas[data] for data in datas]
+    cursor.execute(f"""
+                    INSERT into applicants
+                    VALUES ('{new_datas[0]}', '{new_datas[1]}', '{new_datas[2]}', '{new_datas[3]}', '{new_datas[4]}', '{new_datas[5]}')""")
+
+
+@database_common.connection_handler
+def record_applicants(cursor, a_code):
+    cursor.execute(f"""
+                    SELECT * FROM applicants
+                    where application_code = {a_code}""")
+    names = cursor.fetchall()
+    return names
+
+@database_common.connection_handler
+def update_applicant_record(cursor, what, for_what, person_first_name, person_last_name):
+    cursor.execute(f"""
+                    UPDATE applicants
+                    set {what} =  '{for_what}'
+                    where first_name = '{person_first_name}' and last_name = '{person_last_name}'""")
+
+
+@database_common.connection_handler
+def cancel(cursor, email):
+    cursor.execute(f"""
+                    DELETE from applicants
+                    where email like '%{email}%'""")
--- main.py
+++ main.py
@@ -1,21 +1,70 @@
 import data_manager
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-
-@app.route('/')
+@app.route('/tables')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        if request.form.get('choose'):
+            data_table = request.form.get('choose')
+            requested_table_datas = data_manager.get_tables(data_table)
+            return render_template('tables.html', requested_table_datas=requested_table_datas)
+        if request.form.get('nickn'):
+            town = request.form.get('nickn')
+            requested_datas = data_manager.nicknames(town)
+            return render_template('nicknames.html', requested_datas=requested_datas)
+        if request.form.get('info'):
+            info_box = {'id': request.form.get('id'),
+                        'first_name': request.form.get('first_name'),
+                        'last_name': request.form.get('last_name'),
+                        'phone_number': request.form.get('phone_number'),
+                        'email': request.form.get('email'),
+                        'application_code': request.form.get('application_code')}#appl.code-nál nem működik, mert számot vár és str-et kap.
+            appl_info = request.form.get('info')
+            search = data_manager.search_for_applicant(info_box, appl_info)
+            return render_template('search_applicants.html', search=search)
+        if request.form.get('e_mail'):
+            new_infos = {'id': request.form.get('id'),
+                        'first_name': request.form.get('first_name'),
+                        'last_name': request.form.get('last_name'),
+                        'phone_number': request.form.get('phone_number'),
+                        'email': request.form.get('e_mail'),
+                        'application_code': request.form.get('application_code')}
+            data_manager.add_new_applicant(new_infos)
+            return redirect('/')
+        if request.form.get('record_for_applicant'):
+            code = request.form.get('record_for_applicant')
+            record = data_manager.record_applicants(code)
+            return render_template('all_record_for_one.html', record=record)
+        if request.form.get('update_f_n'):
+            first_name = request.form.get('update_f_n')
+            last_name = request.form.get('update_l_n')
+            which = request.form.get('update_which')
+            changed_data = request.form.get('update_data')
+            data_manager.update_applicant_record(which, changed_data, first_name, last_name)
+            return redirect('/')
+        if request.form.get('cancel_applicant'):
+            e_mail = request.form.get('cancel_applicant')
+            data_manager.cancel(e_mail)
+            return redirect('/')
     return render_template('index.html')
 
 
+@app.route('/nicknames')
+def nick_names():
+    n_n = data_manager.nicknames()
+    return render_template('nicknames.html', n_n=n_n)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+'''
 @app.route('/mentors-with-best-first-name')
 def mentor_names():
     # We get back dictionaries here (for details check 'database_common.py')
     mentor_names = data_manager.get_mentor_names_by_first_name('László')
 
     return render_template('mentor_names.html', mentor_names=mentor_names)
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+'''
